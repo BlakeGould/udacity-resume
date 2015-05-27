550a25f1a75908a8a939ef91f3d871a349774312
Blake Gould <gouldbf@gmail.com>
2015-05-27 13:44:52 -0400

    Append employer and job titles to page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -108,7 +108,16 @@ if (bio.skills.length >0) {
   $("#skills").append(formattedSkill);
 }
 
+for (job in work.jobs) {
+  $("#workExperience").append(HTMLworkStart);
 
+  var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+  var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+  var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+  $(".work-entry:last").append(
+    formattedEmployerTitle);
+}
 
 
 
