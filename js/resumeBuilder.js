eee6324560734bc3facd7000344eb1f4ed0fa88b
Blake Gould <gouldbf@gmail.com>
2015-05-27 15:40:49 -0400

    Create displayWork function to add work info to page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -118,23 +118,26 @@ if (bio.skills.length >0) {
 //     formattedSkill);
 // }
 
-for (job in work.jobs) {
-  // create new div for work experience
-  $("#workExperience").append(HTMLworkStart);
-  // concat employer and title
-  var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-  var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-  var formattedEmployerTitle = formattedEmployer + formattedTitle;
-  $(".work-entry:last").append(
-    formattedEmployerTitle);
+function displayWork() {
+  for (job in work.jobs) {
+    // create new div for work experience
+    $("#workExperience").append(HTMLworkStart);
+    // concat employer and title
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+    $(".work-entry:last").append(
+      formattedEmployerTitle);
 
-  var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-  $(".work-entry:last").append(formattedDates);
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    $(".work-entry:last").append(formattedDates);
 
-  var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-  $(".work-entry:last").append(formattedDescription);
+    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+    $(".work-entry:last").append(formattedDescription);
+  }
 }
 
+displayWork();
 
 
 
