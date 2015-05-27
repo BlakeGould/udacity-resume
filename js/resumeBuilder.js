366fb930ead6589aeda99eb38886d7bfa192501d
Blake Gould <gouldbf@gmail.com>
2015-05-27 15:34:39 -0400

    Append work dates and description to page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -108,15 +108,31 @@ if (bio.skills.length >0) {
   $("#skills").append(formattedSkill);
 }
 
-for (job in work.jobs) {
-  $("#workExperience").append(HTMLworkStart);
 
+// for (skill in bio.skills) {
+//   $("#header").append(HTMLskillsStart);
+
+//   var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+
+//   $("#skills:last").append(
+//     formattedSkill);
+// }
+
+for (job in work.jobs) {
+  // create new div for work experience
+  $("#workExperience").append(HTMLworkStart);
+  // concat employer and title
   var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
   var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
   var formattedEmployerTitle = formattedEmployer + formattedTitle;
-
   $(".work-entry:last").append(
     formattedEmployerTitle);
+
+  var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+  $(".work-entry:last").append(formattedDates);
+
+  var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+  $(".work-entry:last").append(formattedDescription);
 }
 
 
